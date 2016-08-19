2279120449d91675a57e72992c93be38d001f141
jtonic <antonel.pazargic@gmail.com>
2016-08-19 21:49:26 +0300

    A very simple Future example

--- src/main/scala/ScalaSchool.scala
+++ src/main/scala/ScalaSchool.scala
@@ -1,13 +1,49 @@
 /**
   * Created by antonelpazargic on 19/08/16.
   */
-object ScalaSchool extends App {
+import scala.concurrent.Future
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
-  val opt: Option[Int] = None
-  val opt2: Option[Int] = Some(100)
+object ScalaSchool {
+  def main(args: Array[String]) = {
+    futureExample()
 
-  println(opt getOrElse -1)
-  println(opt2 getOrElse -1)
-  println(opt2 get)
-  println(opt2 orElse None)
+    System.exit(0)
+    optionExample
+  }
+
+  def futureExample() = {
+
+    val op1 = 1
+    val op2 = 10
+
+    val computeTheSum: Future[Int] = Future {
+      throw new RuntimeException("Throwing an exception")
+//      op1 + op2
+    }
+
+    computeTheSum onSuccess {
+      case msg => println(msg)
+    }
+
+    computeTheSum onFailure {
+      case exc: Exception => println(exc.getMessage)
+    }
+
+    Await.ready(computeTheSum, 1 seconds)
+
+  }
+
+  def optionExample = {
+    val opt: Option[Int] = None
+    val opt2: Option[Int] = Some(100)
+
+    println(opt getOrElse -1)
+    println(opt2 getOrElse -1)
+    println(opt2 get)
+    println(opt2 orElse None)
+  }
 }
+
